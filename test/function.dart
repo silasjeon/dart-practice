28716427e03bfefddd6015f2659d1bae4e62d5d8
silasjeon <hatching@gmail.com>
2021-03-23 02:08:15 +0900

    Function named parameter
    - Update minimum sdk to 2.12 to enable 'non-nullable' language feature

--- test/function.dart
+++ test/function.dart
@@ -6,6 +6,42 @@ void main() {
       print(string);
     }
 
-    expect(true, printOne is Function);
+    expect(printOne is Function, true);
+  });
+
+  test('function can be passed as parameter ', () {
+    int run(func) {
+      return func(3);
+    }
+
+    var multiply = (x) => x * 2;
+
+    expect(run(multiply), 6);
+  });
+
+  test('defining function with named parameters', () {
+    int enableFlags ({bool? bold, bool? hidden}) {
+      return 3;
+    };
+
+    expect(enableFlags(), 3); // named parameters are optional
+    expect(enableFlags(bold: true), 3);
+    expect(enableFlags(hidden: false, bold: true), 3);
+  });
+
+  test('non nullable named parameters should be given default values', () {
+    int enableFlags ({bool bold=true, bool hidden=false}) {
+      return 3;
+    };
+
+    expect(enableFlags(), 3);
+  });
+
+  test('you can specify "required" for named parameter', () {
+    int enableFlags ({required bool bold, bool hidden=false}) {
+      return 3;
+    };
+
+    expect(enableFlags(bold: true), 3);
   });
 }
