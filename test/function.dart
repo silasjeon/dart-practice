e4e37f118f7ec9ec77ad6b391bf8bcbdd532b49c
silasjeon <hatching@gmail.com>
2021-03-25 00:34:19 +0900

    More function

--- test/function.dart
+++ test/function.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('function is also an object ', () {
-    void printOne(string) {
+    void printOne(string, {String? two}) {
       print(string);
     }
 
@@ -20,9 +20,11 @@ void main() {
   });
 
   test('defining function with named parameters', () {
-    int enableFlags ({bool? bold, bool? hidden}) {
+    int enableFlags({bool? bold, bool? hidden}) {
       return 3;
-    };
+    }
+
+    ;
 
     expect(enableFlags(), 3); // named parameters are optional
     expect(enableFlags(bold: true), 3);
@@ -30,18 +32,60 @@ void main() {
   });
 
   test('non nullable named parameters should be given default values', () {
-    int enableFlags ({bool bold=true, bool hidden=false}) {
+    int enableFlags({bool bold = true, bool hidden = false}) {
       return 3;
-    };
+    }
+
+    ;
 
     expect(enableFlags(), 3);
   });
 
   test('you can specify "required" for named parameter', () {
-    int enableFlags ({required bool bold, bool hidden=false}) {
+    int enableFlags({required bool bold, bool hidden = false}) {
       return 3;
-    };
+    }
+
+    ;
 
     expect(enableFlags(bold: true), 3);
+  });
+
+  test('positional parameter is also available', () {
+    int enableFlags(bool first, [bool? bold, bool hidden = false]) {
+      return 3;
+    }
+
+    ;
+
+    expect(enableFlags(true, true, false), 3);
+  });
+
+  test('anonymous function can be created', () {
+    var list = [1, 2, 3];
+    var x = 0;
+    list.forEach((i) {
+      x += i;
+    });
+
+    expect(x, 6);
+
+    var y = 0;
+    list.forEach((i) => y += i);
+    expect(y, 6);
+  });
+
+  test('closure support', () {
+    Function makeAdder(int addBy) {
+      return (int i) => addBy + i;
+    }
+
+    var add2 = makeAdder(2);
+
+    // Create a function that adds 4.
+    var add4 = makeAdder(4);
+
+    assert(add2(3) == 5);
+    assert(add4(3) == 7);
   });
 }
